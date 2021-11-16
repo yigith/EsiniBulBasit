34ab16526e864ad0db1f48a1e06515ba2fe73f17
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-11-16 16:57:56 +0300

    Add project files.

---
+++ EsiniBul/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace EsiniBul
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlButonlar = new System.Windows.Forms.Panel();
+            this.btnTekrarOyna = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pnlButonlar
+            // 
+            this.pnlButonlar.Location = new System.Drawing.Point(12, 12);
+            this.pnlButonlar.Name = "pnlButonlar";
+            this.pnlButonlar.Size = new System.Drawing.Size(400, 400);
+            this.pnlButonlar.TabIndex = 0;
+            // 
+            // btnTekrarOyna
+            // 
+            this.btnTekrarOyna.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTekrarOyna.Location = new System.Drawing.Point(110, 167);
+            this.btnTekrarOyna.Name = "btnTekrarOyna";
+            this.btnTekrarOyna.Size = new System.Drawing.Size(191, 70);
+            this.btnTekrarOyna.TabIndex = 0;
+            this.btnTekrarOyna.Text = "TEKRAR OYNA";
+            this.btnTekrarOyna.UseVisualStyleBackColor = true;
+            this.btnTekrarOyna.Visible = false;
+            this.btnTekrarOyna.Click += new System.EventHandler(this.btnTekrarOyna_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 427);
+            this.Controls.Add(this.btnTekrarOyna);
+            this.Controls.Add(this.pnlButonlar);
+            this.Name = "Form1";
+            this.Text = "Eşini Bul";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlButonlar;
+        private System.Windows.Forms.Button btnTekrarOyna;
+    }
+}
+
---
+++ EsiniBul/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsiniBul
+{
+    public partial class Form1 : Form
+    {
+        List<Button> butonlar;
+        List<Button> aciklar;
+        const string sembolHavuzu = "$₺€¥₾₽₪₦";
+        char[] semboller;
+        int kapananAdet = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ButonlariOlustur();
+            SembolleriOlustur();
+        }
+
+        private void SembolleriOlustur()
+        {
+            semboller = new char[16];
+            Array.Copy(sembolHavuzu.ToArray(), semboller, 8);
+            Array.Copy(sembolHavuzu.ToArray(), 0, semboller, 8, 8);
+            Karistir(semboller);
+        }
+
+        private void Karistir(char[] semboller)
+        {
+            Random rnd = new Random();
+            char temp;
+            int talihliIndeks;
+            for (int i = 0; i < semboller.Length - 1; i++)
+            {
+                talihliIndeks = rnd.Next(i, semboller.Length);
+                temp = semboller[i];
+                semboller[i] = semboller[talihliIndeks];
+                semboller[talihliIndeks] = temp;
+            }
+        }
+
+        private void ButonlariOlustur()
+        {
+            aciklar = new List<Button>();
+            butonlar = new List<Button>();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Button btn = new Button();
+                    btn.Size = new Size(90, 90);
+                    btn.Font = new Font("Times New Roman", 24f);
+                    btn.Location = new Point(j * 100, i * 100);
+                    btn.Click += Btn_Click;
+                    pnlButonlar.Controls.Add(btn);
+                    butonlar.Add(btn);
+                }
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button tiklanan = (Button)sender;
+            if (aciklar.Contains(tiklanan)) return; // zaten açıksa metottan çık
+
+            if (aciklar.Count == 2) // önceden açık olan 2 adet kart varsa, kapat
+                AcikKartlariKapat();
+
+            // tıklanan kartın sembolünü göster
+            aciklar.Add(tiklanan);
+            int i = butonlar.IndexOf(tiklanan);
+            tiklanan.Text = semboller[i].ToString();
+
+            // eğer 2. kart açıldıysa ve aynıysa
+            if (aciklar.Count == 2 && aciklar[0].Text == aciklar[1].Text)
+            {
+                Refresh(); // metot sonlanmadan arayüzü tazele (açılanları görmek için)
+                Thread.Sleep(500); // gizlemeden önce yarım saniye görsün
+                AcikKartlariKapatVeGizle();
+                kapananAdet += 2;
+            }
+
+            // hiç görünen kart kalmadıysa
+            if (kapananAdet == 16)
+            {
+                MessageBox.Show("Oyun bitti!");
+                btnTekrarOyna.Show();
+            }
+        }
+
+        private void AcikKartlariKapat()
+        {
+            foreach (Button button in aciklar)
+            {
+                button.Text = "";
+            }
+            aciklar.Clear();
+        }
+
+        private void AcikKartlariKapatVeGizle()
+        {
+            foreach (Button button in aciklar)
+            {
+                button.Text = "";
+                button.Hide();
+            }
+            aciklar.Clear();
+        }
+
+        private void btnTekrarOyna_Click(object sender, EventArgs e)
+        {
+            pnlButonlar.Controls.Clear();
+            ButonlariOlustur();
+            SembolleriOlustur();
+            kapananAdet = 0;
+            aciklar.Clear();
+            btnTekrarOyna.Hide();
+        }
+    }
+}
